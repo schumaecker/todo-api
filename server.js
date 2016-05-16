f6d3a1c9e8c50c83fea5f1e05754674f9334fc22
Matthew Schumaecker <matthew.schumaecker@gmail.com>
2016-05-16 17:56:45 -0400

    put added

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get('/todos/:id', function(req,res){
 
 //POST
 app.post('/todos', function(req, res){
+	
 	var body = _.pick(req.body, "description", "completed");
 	//use _.pick to only pick description and completed
 	//set body description to trim value
@@ -73,7 +74,37 @@ app.delete('/todos/:id', function(req, res){
 		todos = _.without(todos, matchedToDelete);
 		res.json(matchedToDelete);
 	}
-})
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') 
+		&& _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') 
+		&& _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT);
