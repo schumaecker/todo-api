cebadd1b5e0a5c49a35951bd574dce3eea44f8bd
Matthew Schumaecker <matthew.schumaecker@gmail.com>
2016-05-19 22:10:02 -0400

    post to db

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-
+var db = require('./db.js');
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [];
+var todos = db.todo;
 var todoNextId = 1;
 
 app.use(bodyParser.json());
@@ -63,16 +63,22 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
 
-	todos.push(body);
+	// todos.push(body);
 
-	res.json(body);
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
+
+	// res.json(body);
 });
 
 
@@ -123,6 +129,7 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
+db.sequelize.sync();
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT);
